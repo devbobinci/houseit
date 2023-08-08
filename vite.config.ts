14d86801999c0f6081644fef92f373dab40c1c74
imxbartus <bartoslaw5@gmail.com>
2023-08-08 20:06:42 +0200

    changed nav icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const ASSET_URL = process.env.ASSET_URL || "";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: "/",
-    server: { port: 3000 },
   };
 
   if (command !== "serve") {
