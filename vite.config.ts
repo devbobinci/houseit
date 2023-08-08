e862093db193db230d28289d1960bb9c2c330bef
imxbartus <bartoslaw5@gmail.com>
2023-08-08 19:32:43 +0200

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ const ASSET_URL = process.env.ASSET_URL || "";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${ASSET_URL}/dist`,
   plugins: [react()],
   server: { port: 3000 },
 });
