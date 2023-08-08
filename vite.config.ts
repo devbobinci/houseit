bd14146ab0f11bf7457a9bd7a9917b1211adc30b
imxbartus <bartoslaw5@gmail.com>
2023-08-08 19:37:09 +0200

    trying to deploy7

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ const ASSET_URL = process.env.ASSET_URL || "";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://imxbartus.github.io/real-estate-houseit",
   plugins: [react()],
   server: { port: 3000 },
 });
