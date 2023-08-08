a6bf009f0dae82109c66ba0a00a1725024aaebcb
imxbartus <bartoslaw5@gmail.com>
2023-08-08 19:18:15 +0200

    trying to deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://imxbartus.github.io/real-estate-houseit/",
   plugins: [react()],
   server: { port: 3000 },
 });
