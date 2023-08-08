ebd0b67d3bffcbae602334d734a5a66db621ed82
imxbartus <bartoslaw5@gmail.com>
2023-08-08 19:28:13 +0200

    trying to deploy5

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const ASSET_URL = process.env.ASSET_URL || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}/dist`,
   plugins: [react()],
   server: { port: 3000 },
 });
